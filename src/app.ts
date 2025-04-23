1b06e40b094a89360eb713df9e72c1e56a32e02f
FireFlyDeveloper <saludeskimdev@gmail.com>
2025-04-24 00:51:20 +0800

    add HTML serving functionality and create login/register pages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { serveStatic } from 'hono/bun';
+import { serveHTML } from './helpers/serverHTML';
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.use('/static/*', serveStatic({ root: './src/' }))
+
+app.get('/login', (c) => c.html(serveHTML('login.html')));
+app.get('/register', (c) => c.html(serveHTML('register.html')));
 
 export default app;
---
+++ src/helpers/serverHTML.ts
@@ -0,0 +1,10 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+function serveHTML(fileName: string) {
+    const filePath = join(process.cwd(), 'src/html', fileName);
+    const html = readFileSync(filePath, 'utf8');
+    return html;
+}
+
+export { serveHTML };
